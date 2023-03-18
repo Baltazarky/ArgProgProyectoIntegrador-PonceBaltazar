8866b2ef0cf7de2d49cc9b4b3561da5422aa8c36
Baltazarky <61981650+Baltazarky@users.noreply.github.com>
2023-03-18 19:11:19 -0300

    Header hecho!

--- Portfolio_PonceBaltazar/src/app/app.module.ts
+++ Portfolio_PonceBaltazar/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgProgComponent } from './components/logo-arg-prog/logo-arg-prog.component';
+import { SocialsComponent } from './components/socials/socials.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgProgComponent,
+    SocialsComponent,
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
