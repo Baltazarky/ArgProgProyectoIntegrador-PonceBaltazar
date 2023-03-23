7a100f5f241ab8dfa7c782e55384d1c79fda2152
Baltazarky <61981650+Baltazarky@users.noreply.github.com>
2023-03-23 19:52:02 -0300

    Routing & login
    hace falta modificar el html y css del login

--- Portfolio_PonceBaltazar/FrontEnd/src/app/components/socials/socials.component.ts
+++ Portfolio_PonceBaltazar/FrontEnd/src/app/components/socials/socials.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-socials',
   templateUrl: './socials.component.html',
   styleUrls: ['./socials.component.css']
 })
-export class SocialsComponent {
+export class SocialsComponent implements OnInit {
+  constructor(private router:Router) { }
 
+  ngOnInit(): void {
+    
+  }
+
+  login(){
+    this.router.navigate(['/login'])
+  }
 }
