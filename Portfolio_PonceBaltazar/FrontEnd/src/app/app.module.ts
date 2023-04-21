e88ac27d4344b6031725f091ea144b9538c14dd4
Baltazarky <61981650+Baltazarky@users.noreply.github.com>
2023-04-21 17:39:01 -0300

    Experiencia

--- Portfolio_PonceBaltazar/FrontEnd/src/app/app.module.ts
+++ Portfolio_PonceBaltazar/FrontEnd/src/app/app.module.ts
@@ -19,6 +19,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { interceptorProvider } from './service/interceptor-service';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 
 
 @NgModule({
@@ -35,7 +37,9 @@ import { interceptorProvider } from './service/interceptor-service';
     ProyectosComponent,
     FooterComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    NewExperienciaComponent,
+    EditExperienciaComponent
   ],
   imports: [
     BrowserModule,
