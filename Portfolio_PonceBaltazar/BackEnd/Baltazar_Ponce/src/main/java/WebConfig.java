e5819dda822e3e876f8d2a4e23b8e42ef9df1e64
Baltazarky <61981650+Baltazarky@users.noreply.github.com>
2023-04-27 18:15:40 -0300

    otro intento más

--- Portfolio_PonceBaltazar/BackEnd/Baltazar_Ponce/src/main/java/WebConfig.java
+++ Portfolio_PonceBaltazar/BackEnd/Baltazar_Ponce/src/main/java/WebConfig.java
@@ -1,20 +1,27 @@
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://balponargprog.web.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedOrigins("https://backend-mgub.onrender.com")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new CustomInterceptor());
+  }
 }
+
