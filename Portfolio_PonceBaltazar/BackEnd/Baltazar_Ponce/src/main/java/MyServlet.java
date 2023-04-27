b3ed5f9b31d0142da770b6174696ae2f7713bc00
Baltazarky <61981650+Baltazarky@users.noreply.github.com>
2023-04-27 17:16:03 -0300

    y va otro intento

---
+++ Portfolio_PonceBaltazar/BackEnd/Baltazar_Ponce/src/main/java/MyServlet.java
@@ -0,0 +1,15 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet {
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    resp.addHeader("Access-Control-Allow-Origin", "*");
+    resp.addHeader("Content-Type", "text/csv");
+      CharSequence csvString = null;
+    resp.getWriter().append(csvString);
+  }
+}
--- Portfolio_PonceBaltazar/BackEnd/Baltazar_Ponce/src/main/java/com/ArgProyecto/Baltazar_Ponce/BaltazarPonceApplication.java
+++ Portfolio_PonceBaltazar/BackEnd/Baltazar_Ponce/src/main/java/com/ArgProyecto/Baltazar_Ponce/BaltazarPonceApplication.java
@@ -9,17 +9,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class BaltazarPonceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BaltazarPonceApplication.class, args);
-	}
-        
-       @Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/auth/login").allowedOrigins("https://balponargprog.web.app", "https://balponargprog.web.app/login");
-			}
-		};
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BaltazarPonceApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://balponargprog.web.app/", "https://backend-mgub.onrender.com")
+                        .allowedMethods("*");
+            }
+        };
+    }
 }
