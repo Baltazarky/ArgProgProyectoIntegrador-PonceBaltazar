fe7681a98c6edf8a4a97bad349b21e9e5276f667
Baltazarky <61981650+Baltazarky@users.noreply.github.com>
2023-05-01 15:02:02 -0300

    Me parece que ahora si

--- Portfolio_PonceBaltazar/BackEnd/Baltazar_Ponce/src/main/java/com/ArgProyecto/Baltazar_Ponce/Security/Controller/AuthController.java
+++ Portfolio_PonceBaltazar/BackEnd/Baltazar_Ponce/src/main/java/com/ArgProyecto/Baltazar_Ponce/Security/Controller/AuthController.java
@@ -36,7 +36,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin (origins = "https://balponargprog.web.app")
+@CrossOrigin(origins = "https://balponargprog.web.app")
 public class AuthController {
 
     @Autowired
