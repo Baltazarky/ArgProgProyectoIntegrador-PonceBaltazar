d7467c5057f3d84a06c6377d4670849b8963ffbe
Baltazarky <61981650+Baltazarky@users.noreply.github.com>
2023-05-03 12:04:53 -0300

    h1

--- Portfolio_PonceBaltazar/BackEnd/Baltazar_Ponce/src/main/java/com/ArgProyecto/Baltazar_Ponce/Security/Controller/AuthController.java
+++ Portfolio_PonceBaltazar/BackEnd/Baltazar_Ponce/src/main/java/com/ArgProyecto/Baltazar_Ponce/Security/Controller/AuthController.java
@@ -37,7 +37,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     @Autowired
