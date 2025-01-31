49260a5a69f921a8786451b474e84e8490cd6b89
Baltazarky <61981650+Baltazarky@users.noreply.github.com>
2025-01-31 17:23:04 -0300

    Hipervinculos en imágenes de proyectos

--- Portfolio_PonceBaltazar/FrontEnd/src/app/components/proyectos/proyectos.component.ts
+++ Portfolio_PonceBaltazar/FrontEnd/src/app/components/proyectos/proyectos.component.ts
@@ -13,24 +13,27 @@ export class ProyectosComponent implements OnInit  {
   containers = [
     {
       title: 'Portfolio Argentina Programa',
-      description: 'El portfolio personal de YoProgramo del curso de Programador Web Full Stack de Argentina Programa es mi único proyecto concluido hasta la fecha.',
-      image: '../../../assets/APLogo_purple.png'
+      description: 'El portfolio personal de YoProgramo del curso de Programador Web Full Stack de Argentina Programa es mi primer proyecto concluido.',
+      image: '../../../assets/APLogo_purple.png',
+      link: 'https://github.com/Baltazarky/ArgProgProyectoIntegrador-PonceBaltazar'
     },
     {
       title: 'Hesoyam',
-      subtitle: 'Sistema de trading automatizado (Binance Futures)',
       description: 'Sistema hecho en Python usando principalmente las librerias de Binance y TradingView. Cuenta con dos funciones, una para la alza y otra para la baja...',
-      image: '../../../assets/hesoyam.png'
+      image: '../../../assets/hesoyam.png',
+      link: 'https://github.com/Baltazarky/Hesoyam'
     },
     {
       title: 'Juego Flash C++',
       description: 'Proyecto final de la asignatura de Programación Orientada a Objetos. Pequeño juego hecho con la libreria Flash de C++ inspirado en Megaman.',
-      image: '../../../assets/rabbit_sprite.png'
+      image: '../../../assets/rabbit_sprite.png',
+      link: 'https://github.com/Baltazarky/Juego-SFMLCpp-POO'
     },
     {
-      title: 'Deep Colors',
+      title: 'Colores Profundos',
       description: 'Proyecto resultado de mi primer GlobalGameJam cuya temática anual era "Burbujas". Asumí el rol de programador donde aprendí a usar Unity.',
-      image: '../../../assets/yellow_submarine.png'
+      image: '../../../assets/yellow_submarine.png',
+      link: 'https://globalgamejam.org/games/2025/colores-profundos-5'
     }
   ];
   currentIndex = 0;
